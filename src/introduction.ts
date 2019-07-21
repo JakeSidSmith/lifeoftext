b6c7b9f03897a451e63ccbb3e2690bbde22ca760
Jake 'Sid' Smith <jake@dabapps.com>
2019-07-21 16:22:16 +0100

    Reuse displayLines util for introduction

--- src/introduction.ts
+++ src/introduction.ts
@@ -1,19 +1,10 @@
-import { NBSP } from './constants';
+import { displayLines } from './utils';
 
-const INTRODUCTION = [
-  'Welcome! To play the game simply type what you would like to do, for example:',
-  'I walk forward.',
-  'I look around.',
-  'I eat an apple.',
-  NBSP,
-];
+const INTRODUCTION =
+  'Welcome! To play the game simply type what you would like to do, for example:\nI walk forward.\nI look around.\nI eat an apple.\n';
 
 const introduction = (messages: HTMLDivElement) => {
-  INTRODUCTION.forEach(text => {
-    const element = document.createElement('p');
-    element.textContent = text;
-    messages.appendChild(element);
-  });
+  displayLines(messages, INTRODUCTION);
 };
 
 export { introduction };
