f0396a92e33a94a306774ce77c0aaf9024fe2ba6
Jake 'Sid' Smith <jake@dabapps.com>
2019-07-21 17:33:47 +0100

    Handle global matches if no match is found

--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,6 @@
 import * as npl from 'compromise';
 
+import { GLOBAL_MATCHES } from './global-matches';
 import { Answer, Prompt } from './types';
 import { displayLines } from './utils';
 
@@ -31,7 +32,24 @@ const play = (
       const match = npl(userInput);
 
       if (match.has(answer.match)) {
-        return advance(answer);
+        advance(answer);
+        return;
+      }
+    }
+
+    for (const globalMatch of GLOBAL_MATCHES) {
+      const match = npl(userInput);
+
+      if (match.has(globalMatch.match)) {
+        const response = globalMatch.respond(match.match(globalMatch.match));
+        if (typeof response === 'string') {
+          displayLines(messages, response);
+          return;
+        }
+
+        history.push(response);
+        displayLines(messages, response.prompt, response.color);
+        return;
       }
     }
 
